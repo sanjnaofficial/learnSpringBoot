ab4e4f5e0cdb65a50577ae95f50125280de8e470
Shrinitha1724 <shrinitha@kritter.in>
2025-06-12 13:11:51 +0000

    Fetch table data and create entity

--- dvd-rental-app/src/main/java/com/example/dvdRentalApp/entity/Actor.java
+++ dvd-rental-app/src/main/java/com/example/dvdRentalApp/entity/Actor.java
@@ -1,27 +1,29 @@
 package com.example.dvdRentalApp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "actor")
+@Table(name = "actor",
+        indexes = {
+                @Index(name = "idx_actor_last_name", columnList = "last_name")
+        })
 public class Actor {
+
     @Id
-    @Column(name = "actor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "actor_id", nullable = false)
     private Integer actorId;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false, length = 45)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
 
-    @Column(name = "last_update")
+    @Column(name = "last_update", nullable = false)
     private LocalDateTime lastUpdate;
 }
